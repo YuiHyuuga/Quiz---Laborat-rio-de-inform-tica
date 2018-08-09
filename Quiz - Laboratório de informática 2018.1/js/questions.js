a2b7061ea569669a7902a461f224c8fff7cb5b4e
YuiHyuuga <41643666+YuiHyuuga@users.noreply.github.com>
2018-08-09 11:40:07 -0300

    Add files via upload

---
+++ Quiz - Laboratório de informática 2018.1/js/questions.js
@@ -0,0 +1,51 @@
+﻿var questions = [{
+  question: "De acordo com a palestra ministrada pelo Prof. Marcelo Gama, o que são Cifras de substituição? ",
+  choices: ['Processo através do qual mensagens são transformadas em textos cifrados', 'Esquema criptográfico onde cada letra é substituida por outra letra ou símbolo', 'Modelo de cifragem mecânica que foi utilizada pelos alemães', 'Modelo de criptografia que não utiliza diretamente a estrutura dos números inteiros'],
+  correctAnswer: 1,
+  answerLocation: '"https://drive.google.com/file/d/1udC6rm1hWeA9orxuhOWmOkDjL71lNLQb/view"'
+}, {
+  question: "O que é Realidade Virtual?",
+  choices: ['Informações virtuais sobrepostas à realidade em uma tela comum','Sistema incapaz de isolar o usuário do mundo externo','Tecnologia capaz de transferir a consciencia do usuário para um ambiente virtual','Ecossistema de tecnologias e linguagens capaz de envolver um usuário em um ambiente digitalmente simulado'],
+  correctAnswer: 3,
+  answerLocation: '"https://drive.google.com/file/d/1THZ7XEG3l2xQ5pqZiz_Z2mm9wiHUt1nC/view"'
+}, {
+  question: "Sobre computação evolucionária, assinale a alternativa INCORRETA: ",
+  choices: ['Computação Evolucionária não é parte de Ciência da computação', 'Computação Evolucionária não é parte da biologia', 'A biologia cedeu a inspiração e a terminologia', 'Computação Evolucionária pode ser aplicada em pesquisas biológicas'],
+  correctAnswer: 0,
+  answerLocation: '"https://drive.google.com/file/d/14z4Bwy6FkdYjOlZdMvPRklqAtDPsU-rK/view"'
+}, {
+  question: "Definição de rastreamento segundo a ISO 9001:2000 :",
+  choices: ['Processo que engloba atividades contribuintes para a produção de requisitos', 'Verificação de artefatos de software e seus relacionamentos', 'Capacidade de rastrear a história, aplicação ou localização do que está sendo considerado', 'Conjunto de atividades exercidas ao longo do projeto'],
+  correctAnswer: 2,
+  answerLocation: '"https://drive.google.com/file/d/1fj-TR97EEfkrBagE1E02Iq77Is9Iw6he/view"'
+}, {
+  question: "São atividades que compõem o processo da engenharia de requisitos, exceto: ",
+  choices: ['Validação', 'Análise e negociação', 'Identificação ou levantamento', 'Execução'],
+  correctAnswer: 3,
+  answerLocation: '"https://drive.google.com/file/d/1fj-TR97EEfkrBagE1E02Iq77Is9Iw6he/view"'
+}, {
+  question: "Segundo o seminário de Croptografia, quem foi Diffie e Hellman? ",
+  choices: ['Criadores de uma cifragem mecânica utilizada na segunda guerra mundial', 'Autores do artigo mais importante da história da criptografia', 'Criaram, em 1977, o primeiro modelo de esquema criptográfico de chave pública', 'Criadores do modelo criptográfico que não utiliza a estrutura dos números inteiros'],
+  correctAnswer: 1,
+  answerLocation: '"https://drive.google.com/file/d/1udC6rm1hWeA9orxuhOWmOkDjL71lNLQb/view"'
+}, {
+  question: "São perigos da Realidade Virtual, exceto: ",
+  choices: ['Problemas cardiovasculares', 'Vício', 'Comportamento negativo associado ao tempo de exposição', 'Falta de contato com a natureza'],
+  correctAnswer: 0,
+  answerLocation: '"https://drive.google.com/file/d/1THZ7XEG3l2xQ5pqZiz_Z2mm9wiHUt1nC/view"'
+}, {
+  question: "São benefícios da Realidade Virtual, exceto: ",
+  choices: ['Entretenimento', 'Fins Terapêuticos', 'Fins Educacionais', 'Socialização'],
+  correctAnswer: 3,
+  answerLocation: '"https://drive.google.com/file/d/1THZ7XEG3l2xQ5pqZiz_Z2mm9wiHUt1nC/view"'
+}, {
+  question: "São áreas onde o Rastreamento está envolvido: ",
+  choices: ['Veicular, Alimentos, Medicamentos, Engenharia', 'Financeira, Alimentos, Comercial, Engenharia', 'Veicular, Alimentos, Design, Engenharia', 'Alimentos, Medicamentos'],
+  correctAnswer: 0,
+  answerLocation: '"https://drive.google.com/file/d/1fj-TR97EEfkrBagE1E02Iq77Is9Iw6he/view"'
+}, {
+  question: "Não é um tipo de Algoritmo Evolucionário: ",
+  choices: ['Strings Binárias: Algoritmos Genéticos', 'Vetores de valores reais: Estratégias Evolutivas', 'Classes e objetos: Algoritmos Objetivos', 'Árvores LISP: Programação Genética'],
+  correctAnswer: 2,
+  answerLocation: '"https://drive.google.com/file/d/14z4Bwy6FkdYjOlZdMvPRklqAtDPsU-rK/view"'
+}];
